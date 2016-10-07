1b82f300f08ad1384713f356ff94965a5e2f852a
Administrator <Administrator@10.2.11.73>
2016-10-07 14:09:14 +0700

    GamePacmam

---
+++ core/src/com/jittat/gdx/game/PacmanGame.java
@@ -0,0 +1,26 @@
+package com.jittat.gdx.game;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public class PacmanGame extends ApplicationAdapter {
+	public SpriteBatch batch;
+	
+	@Override
+	public void create () {
+		batch = new SpriteBatch();
+	}
+
+	@Override
+	public void render () {
+		super.render();
+	}
+	
+	@Override
+	public void dispose () {
+		batch.dispose();
+	}
+}
